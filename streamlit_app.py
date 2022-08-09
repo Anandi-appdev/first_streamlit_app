fda3a51137a141d48b4ebbcce8039dc65b9ae74e
Anandi-appdev <110664982+Anandi-appdev@users.noreply.github.com>
2022-08-09 18:47:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,8 @@ streamlit.dataframe(my_fruit_list)
 #################################################################################
 ###### New Section to disply fruityvice api response ################
 
+streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
